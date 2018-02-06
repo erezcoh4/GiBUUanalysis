ab64ba9d9839133801ddc9f7cf409f80b563d61e
Erez Cohen <erezcohen@Erezs-iMac.local>
2018-02-06 13:25:53 +0200

    Tue Feb  6 13:25:53 IST 2018

---
+++ mac/dat_file_to_csv.py
@@ -0,0 +1,45 @@
+'''
+    GiBUU: read (output) dat file and plug it into a csv file...
+    
+    usage:
+    ---- - -- -- --
+    python mac/dat_file_to_csv.py --infilename=RealParticles_Final_ALL --Nlines=10 -v2
+    python mac/dat_file_to_csv.py --infilename=DoHIA_pert --Nevents=1 -v2
+    python mac/dat_file_to_csv.py --infilename=DoHIA_pert --Nlines=10 -v2
+'''
+import sys, argparse
+sys.path.insert(0, '/Users/erezcohen/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
+import matplotlib as mpl, pandas as pd , numpy as np
+from matplotlib import pyplot as plt
+from my_tools import *
+
+import ROOT
+from ROOT import ReadDatFile
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-path','--path', default='/Users/erezcohen/Desktop/GiBUU/release2017/Ntransparency/', type=str )
+parser.add_argument('-infile','--infilename', default='RealParticles_Init_ALL', type=str )
+parser.add_argument('-v','--verbose', default=0, type=int )
+parser.add_argument('-Nlns','--Nlines', default=-1, type=int )
+parser.add_argument('-Nevt','--Nevents', default=1, type=int )
+
+path = parser.parse_args().path
+infilename = parser.parse_args().infilename
+verbose = parser.parse_args().verbose
+Nlines = parser.parse_args().Nlines
+Nevents = parser.parse_args().Nevents
+
+datfilename = path + infilename + '.dat'
+datfiletype = infilename
+csvfilename = path + infilename + '.csv'
+
+print 'reading\n',datfilename,'\nand converting to\n',csvfilename
+
+rd = ReadDatFile (datfilename,csvfilename,datfiletype)
+
+rd.ReadDatFromFile( Nlines , verbose , Nevents)
+
+print_filename(csvfilename,'wrote to output file')
+print 'done.'
+
